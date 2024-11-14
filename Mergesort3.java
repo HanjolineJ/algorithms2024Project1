4135cbe970757c9ecff2351ff70c2f0427ced4ea
HanjolineJ <Hanjolinej@gmail.com>
2024-11-14 11:23:21 -0500

    Mergesort 3 and 4 with larger instances

--- Mergesort3.java
+++ Mergesort3.java
@@ -7,60 +7,101 @@ public class Mergesort3 {
         int n = S.length;
         U = new int[n];
 
-        mergesort3(n);
-        
+        mergesort3(S, n);
+
         System.out.println("Sorted array: ");
         for (int num : S) {
             System.out.print(num + " ");
         }
+        System.out.println("\n");
+
+        // Additional code for performance testing
+        int[] sizes = {10, 100, 1000, 10000, 100000, 1000000};
+
+        for (int size : sizes) {
+            int[][] testArrays = {
+                ArrayGenerator.generateRandomArray(size),
+                ArrayGenerator.generateSortedArray(size),
+                ArrayGenerator.generateReversedArray(size),
+                ArrayGenerator.generateNearlySortedArray(size, 0.05)
+            };
+
+            String[] arrayTypes = {"Random", "Sorted", "Reversed", "Nearly Sorted"};
+
+            for (int t = 0; t < testArrays.length; t++) {
+                int[] generatedArray = testArrays[t];
+                long totalTime = 0;
+
+                // Run the sort 100 times and calculate the average execution time
+                for (int i = 0; i < 100; i++) {
+                    int[] arrayCopy = generatedArray.clone();
+                    long startTime = System.nanoTime();
+                    mergesort3(arrayCopy, arrayCopy.length);
+                    long endTime = System.nanoTime();
+                    totalTime += (endTime - startTime);
+                }
+
+                long averageTimeNano = totalTime / 100;
+                double averageTimeSeconds = averageTimeNano / 1.0e9; // Convert nanoseconds to seconds
+
+                long minutes = (long) (averageTimeSeconds / 60);
+                double seconds = averageTimeSeconds % 60;
+
+                System.out.println("Average execution time for " + arrayTypes[t] + " array of size " + size + ":");
+                System.out.println("    " + averageTimeNano + " nanoseconds");
+                System.out.println("    " + averageTimeSeconds + " seconds");
+                System.out.println("    " + minutes + " minutes and " + seconds + " seconds");
+            }
+        }
     }
 
-    public static void mergesort3(int n) {
+    // Updated mergesort3 method to accept an array and size as parameters
+    public static void mergesort3(int[] array, int n) {
+        U = new int[n]; // Auxiliary array for merging
+
         // Start with single element arrays and merge iteratively
         for (int width = 1; width < n; width = 2 * width) {
             for (int i = 0; i < n; i = i + 2 * width) {
                 int low = i;
                 int mid = Math.min(i + width - 1, n - 1);
                 int high = Math.min(i + 2 * width - 1, n - 1);
-                
+
                 // Merge the sorted subarrays
-                merge3(low, mid, high);
+                merge3(array, low, mid, high);
             }
-            
-            // Swap roles of U and S
-            int[] temp = S;
-            S = U;
-            U = temp;
+
+            // Copy merged result from U back to array
+            System.arraycopy(U, 0, array, 0, n);
         }
     }
 
-    public static void merge3(int low, int mid, int high) {
+    public static void merge3(int[] array, int low, int mid, int high) {
         int i = low;
         int j = mid + 1;
         int k = low;
-        
+
         // Merge the two subarrays into U
         while (i <= mid && j <= high) {
-            if (S[i] <= S[j]) {
-                U[k] = S[i];
+            if (array[i] <= array[j]) {
+                U[k] = array[i];
                 i++;
             } else {
-                U[k] = S[j];
+                U[k] = array[j];
                 j++;
             }
             k++;
         }
-        
+
         // Copy remaining elements of the left subarray if any
         while (i <= mid) {
-            U[k] = S[i];
+            U[k] = array[i];
             i++;
             k++;
         }
-        
+
         // Copy remaining elements of the right subarray if any
         while (j <= high) {
-            U[k] = S[j];
+            U[k] = array[j];
             j++;
             k++;
         }
--- Mergesort4.java
+++ Mergesort4.java
@@ -1,62 +1,143 @@
+//class Node {
+  //  int key;
+    //Node next;
+
+    //public Node(int key) {
+    //    this.key = key;
+    //    this.next = null;
+    //}
+//}
+
 public class Mergesort4 {
-    static Node listFront;
+    static Node head;
 
     public static void main(String[] args) {
-        // Creating a linked list with some sample values
-        listFront = new Node(16);
-        listFront.next = new Node(14);
-        listFront.next.next = new Node(5);
-        listFront.next.next.next = new Node(7);
-        listFront.next.next.next.next = new Node(1);
-        listFront.next.next.next.next.next = new Node(8);
-        listFront.next.next.next.next.next.next = new Node(12);
-        listFront.next.next.next.next.next.next.next = new Node(10);
-        
-        // Perform the merge sort
-        listFront = mergesort4(listFront);
-        
-        // Print the sorted linked list
-        printList(listFront);
+        int[] array = {16, 14, 5, 7, 1, 8, 12, 10};
+        head = arrayToList(array);
+
+        head = mergesort4(head);
+
+        System.out.println("Sorted linked list: ");
+        printList(head);
+        System.out.println("\n");
+
+        // Additional code for performance testing
+        int[] sizes = {10, 100, 1000, 10000, 100000};
+
+        for (int size : sizes) {
+            int[][] testArrays = {
+                ArrayGenerator.generateRandomArray(size),
+                ArrayGenerator.generateSortedArray(size),
+                ArrayGenerator.generateReversedArray(size),
+                ArrayGenerator.generateNearlySortedArray(size, 0.05)
+            };
+
+            String[] arrayTypes = {"Random", "Sorted", "Reversed", "Nearly Sorted"};
+
+            for (int t = 0; t < testArrays.length; t++) {
+                int[] generatedArray = testArrays[t];
+                long totalTime = 0;
+
+                // Run the sort 10 times and calculate the average execution time
+                for (int i = 0; i < 10; i++) {
+                    Node list = arrayToList(generatedArray.clone());
+                    long startTime = System.nanoTime();
+                    list = mergesort4(list);
+                    long endTime = System.nanoTime();
+                    totalTime += (endTime - startTime);
+                }
+
+                long averageTimeNano = totalTime / 10;
+                double averageTimeSeconds = averageTimeNano / 1.0e9; // Convert nanoseconds to seconds
+
+                long minutes = (long) (averageTimeSeconds / 60);
+                double seconds = averageTimeSeconds % 60;
+
+                System.out.println("Average execution time for " + arrayTypes[t] + " array of size " + size + ":");
+                System.out.println("    " + averageTimeNano + " nanoseconds");
+                System.out.println("    " + averageTimeSeconds + " seconds");
+                System.out.println("    " + minutes + " minutes and " + seconds + " seconds");
+            }
+        }
     }
 
+    // Converts an array to a linked list
+    public static Node arrayToList(int[] array) {
+        if (array.length == 0) {
+            return null;
+        }
+        Node head = new Node(array[0]);
+        Node current = head;
+        for (int i = 1; i < array.length; i++) {
+            current.next = new Node(array[i]);
+            current = current.next;
+        }
+        return head;
+    }
+
+    // Prints the linked list
+    public static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.key + " ");
+            current = current.next;
+        }
+    }
+
+    // Mergesort4 to sort a linked list
     public static Node mergesort4(Node head) {
-        // Base case: if head is null or there is only one element in the list
         if (head == null || head.next == null) {
             return head;
         }
 
         // Split the list into two halves
         Node middle = getMiddle(head);
-        Node nextOfMiddle = middle.next;
+        Node nextToMiddle = middle.next;
         middle.next = null;
 
-        // Recursively sort the two halves
+        // Recursively sort the sublists
         Node left = mergesort4(head);
-        Node right = mergesort4(nextOfMiddle);
+        Node right = mergesort4(nextToMiddle);
 
-        // Merge the sorted halves
+        // Merge the sorted sublists
         return merge4(left, right);
     }
 
-    public static Node merge4(Node list1, Node list2) {
-        Node result;
-        
-        // Base cases
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-
-        // Pick either list1 or list2 and recur
-        if (list1.key <= list2.key) {
-            result = list1;
-            result.next = merge4(list1.next, list2);
-        } else {
-            result = list2;
-            result.next = merge4(list1, list2.next);
+    // Merge two sorted linked lists
+    public static Node merge4(Node left, Node right) {
+        if (left == null) {
+            return right;
         }
-        return result;
+        if (right == null) {
+            return left;
+        }
+
+        Node dummy = new Node(0);
+        Node current = dummy;
+
+        // Merge the two subarrays iteratively
+        while (left != null && right != null) {
+            if (left.key <= right.key) {
+                current.next = left;
+                left = left.next;
+            } else {
+                current.next = right;
+                right = right.next;
+            }
+            current = current.next;
+        }
+
+        // Attach the remaining nodes, if any
+        if (left != null) {
+            current.next = left;
+        } else {
+            current.next = right;
+        }
+
+        return dummy.next;
     }
 
-    // Utility function to get the middle of the linked list
+    // Utility function to find the middle of the linked list
     public static Node getMiddle(Node head) {
         if (head == null) {
             return head;
@@ -64,20 +145,12 @@ public class Mergesort4 {
 
         Node slow = head, fast = head;
 
+        // Move `fast` two nodes and `slow` one node at a time
         while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        return slow;
-    }
 
-    // Utility function to print the linked list
-    public static void printList(Node head) {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.key + " ");
-            temp = temp.next;
-        }
-        System.out.println();
+        return slow;
     }
 }
