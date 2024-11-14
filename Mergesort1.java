f8bc176ccda35db040804fa2fb51f7efd08e1606
HanjolineJ <Hanjolinej@gmail.com>
2024-11-14 11:11:17 -0500

    Mergesort 1 and 2 with bigger instance sizes

--- Mergesort1.java
+++ Mergesort1.java
@@ -1,5 +1,6 @@
 public class Mergesort1 {
     public static void main(String[] args) {
+        // Original main method for testing small hardcoded array
         int[] array = {16, 14, 5, 7, 1, 8, 12, 10};
         mergeSort(array, 0, array.length - 1);
         
@@ -7,58 +8,93 @@ public class Mergesort1 {
         for (int num : array) {
             System.out.print(num + " ");
         }
-    }
+        System.out.println("\n");
 
-    public static void mergeSort(int[] array, int left, int right) {
-        if (left < right) {
-            int middle = (left + right) / 2;
-            
-            // Recursively sort the two halves
-            mergeSort(array, left, middle);
-            mergeSort(array, middle + 1, right);
-            
-            // Merge the sorted halves
-            merge(array, left, middle, right);
+        // Additional code for performance testing
+        int[] sizes = {10, 100, 1000, 10000, 100000, 1000000};
+        
+        for (int size : sizes) {
+            int[][] testArrays = {
+                ArrayGenerator.generateRandomArray(size),
+                ArrayGenerator.generateSortedArray(size),
+                ArrayGenerator.generateReversedArray(size),
+                ArrayGenerator.generateNearlySortedArray(size, 0.05)
+            };
+
+            String[] arrayTypes = {"Random", "Sorted", "Reversed", "Nearly Sorted"};
+
+            for (int t = 0; t < testArrays.length; t++) {
+                int[] generatedArray = testArrays[t];
+                long totalTime = 0;
+
+                // Run the sort 100 times and calculate the average execution time
+                for (int i = 0; i < 100; i++) {
+                    int[] arrayCopy = generatedArray.clone();
+                    long startTime = System.nanoTime();
+                    mergeSort(arrayCopy, 0, arrayCopy.length - 1);
+                    long endTime = System.nanoTime();
+                    totalTime += (endTime - startTime);
+                }
+
+                long averageTimeNano = totalTime / 100;
+                double averageTimeSeconds = averageTimeNano / 1.0e9; // Convert nanoseconds to seconds
+                
+                long minutes = (long) (averageTimeSeconds / 60);
+                double seconds = averageTimeSeconds % 60;
+
+                System.out.println("Average execution time for " + arrayTypes[t] + " array of size " + size + ":");
+                System.out.println("    " + averageTimeNano + " nanoseconds");
+                System.out.println("    " + averageTimeSeconds + " seconds");
+                System.out.println("    " + minutes + " minutes and " + seconds + " seconds");
+            }
         }
     }
 
-    public static void merge(int[] array, int left, int middle, int right) {
-        int n1 = middle - left + 1;
-        int n2 = right - middle;
-        
-        int[] leftArray = new int[n1];
-        int[] rightArray = new int[n2];
-        
-        // Copy data to temporary arrays
-        for (int i = 0; i < n1; ++i)
-            leftArray[i] = array[left + i];
-        for (int j = 0; j < n2; ++j)
-            rightArray[j] = array[middle + 1 + j];
-        
-        // Merge the temporary arrays
-        int i = 0, j = 0;
-        int k = left;
+    // Standard Mergesort method
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+            merge(array, left, mid, right);
+        }
+    }
+
+    // Merge function
+    public static void merge(int[] array, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = array[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
         while (i < n1 && j < n2) {
-            if (leftArray[i] <= rightArray[j]) {
-                array[k] = leftArray[i];
+            if (L[i] <= R[j]) {
+                array[k] = L[i];
                 i++;
             } else {
-                array[k] = rightArray[j];
+                array[k] = R[j];
                 j++;
             }
             k++;
         }
-        
-        // Copy remaining elements of leftArray if any
+
         while (i < n1) {
-            array[k] = leftArray[i];
+            array[k] = L[i];
             i++;
             k++;
         }
-        
-        // Copy remaining elements of rightArray if any
+
         while (j < n2) {
-            array[k] = rightArray[j];
+            array[k] = R[j];
             j++;
             k++;
         }
--- Mergesort2.java
+++ Mergesort2.java
@@ -4,65 +4,107 @@ public class Mergesort2 {
     public static void main(String[] args) {
         S = new int[]{16, 14, 5, 7, 1, 8, 12, 10};
         int n = S.length;
-        
-        mergesort2(0, n - 1);
-        
+
+        mergesort2(S, 0, n - 1);
+
         System.out.println("Sorted array: ");
         for (int num : S) {
             System.out.print(num + " ");
         }
-    }
+        System.out.println("\n");
 
-    public static void mergesort2(int low, int high) {
-        if (low < high) {
-            int mid = (low + high) / 2;
-            
-            // Recursively sort the two halves
-            mergesort2(low, mid);
-            mergesort2(mid + 1, high);
-            
-            // Merge the sorted halves
-            merge2(low, mid, high);
+        // Additional code for performance testing
+        int[] sizes = {10, 100, 1000, 10000, 100000, 1000000};
+
+        for (int size : sizes) {
+            int[][] testArrays = {
+                ArrayGenerator.generateRandomArray(size),
+                ArrayGenerator.generateSortedArray(size),
+                ArrayGenerator.generateReversedArray(size),
+                ArrayGenerator.generateNearlySortedArray(size, 0.05)
+            };
+
+            String[] arrayTypes = {"Random", "Sorted", "Reversed", "Nearly Sorted"};
+
+            for (int t = 0; t < testArrays.length; t++) {
+                int[] generatedArray = testArrays[t];
+                long totalTime = 0;
+
+                // Run the sort 100 times and calculate the average execution time
+                for (int i = 0; i < 100; i++) {
+                    int[] arrayCopy = generatedArray.clone();
+                    long startTime = System.nanoTime();
+                    mergesort2(arrayCopy, 0, arrayCopy.length - 1);
+                    long endTime = System.nanoTime();
+                    totalTime += (endTime - startTime);
+                }
+
+                long averageTimeNano = totalTime / 100;
+                double averageTimeSeconds = averageTimeNano / 1.0e9; // Convert nanoseconds to seconds
+
+                long minutes = (long) (averageTimeSeconds / 60);
+                double seconds = averageTimeSeconds % 60;
+
+                System.out.println("Average execution time for " + arrayTypes[t] + " array of size " + size + ":");
+                System.out.println("    " + averageTimeNano + " nanoseconds");
+                System.out.println("    " + averageTimeSeconds + " seconds");
+                System.out.println("    " + minutes + " minutes and " + seconds + " seconds");
+            }
         }
     }
 
-    public static void merge2(int low, int mid, int high) {
+    // Updated mergesort2 method to accept the array as a parameter
+    public static void mergesort2(int[] array, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+
+            // Recursively sort the two halves
+            mergesort2(array, low, mid);
+            mergesort2(array, mid + 1, high);
+
+            // Merge the sorted halves
+            merge2(array, low, mid, high);
+        }
+    }
+
+    // Updated merge2 method to accept the array as a parameter
+    public static void merge2(int[] array, int low, int mid, int high) {
         int n1 = mid - low + 1;
         int n2 = high - mid;
-        
+
         int[] leftArray = new int[n1];
         int[] rightArray = new int[n2];
-        
+
         // Copy data to temporary arrays
         for (int i = 0; i < n1; ++i)
-            leftArray[i] = S[low + i];
+            leftArray[i] = array[low + i];
         for (int j = 0; j < n2; ++j)
-            rightArray[j] = S[mid + 1 + j];
-        
+            rightArray[j] = array[mid + 1 + j];
+
         // Merge the temporary arrays
         int i = 0, j = 0;
         int k = low;
         while (i < n1 && j < n2) {
             if (leftArray[i] <= rightArray[j]) {
-                S[k] = leftArray[i];
+                array[k] = leftArray[i];
                 i++;
             } else {
-                S[k] = rightArray[j];
+                array[k] = rightArray[j];
                 j++;
             }
             k++;
         }
-        
+
         // Copy remaining elements of leftArray if any
         while (i < n1) {
-            S[k] = leftArray[i];
+            array[k] = leftArray[i];
             i++;
             k++;
         }
-        
+
         // Copy remaining elements of rightArray if any
         while (j < n2) {
-            S[k] = rightArray[j];
+            array[k] = rightArray[j];
             j++;
             k++;
         }
